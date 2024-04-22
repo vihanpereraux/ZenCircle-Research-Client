db77c13748b0946ed81957b3db699a1f553b8630
Vihan Perera <vihanpereraux@gmail.com>
2024-04-22 14:13:21 +0100

    add basic features

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,21 @@
 const userInput = document.getElementById('user-input-text');
 const systemResponse = document.getElementById('system-response');
 const validationText = document.getElementById('validation-text');
-systemResponse.innerText = "I'm  doing good !!!"
+const clearResponse = document.getElementById('clear-response');
+const deleteCatMemory = document.getElementById('delete-chat-memory');
+
+systemResponse.innerText = "Ask something to start the conversation";
+systemResponse.style.opacity = .4;
+systemResponse.style.fontWeight = 400;
 
 const timeoutDelay = 2000;
 
+// function for the generative conversation
 document.getElementById('send-button').addEventListener('click', async () => {
     if (!userInput.value == "") {
         // cleans the previous response
+        systemResponse.style.opacity = 1;
+        systemResponse.style.fontWeight = 400;
         systemResponse.value = "Generating ...";
 
         const URL = 'http://localhost:5000/get-response?content=' + userInput.value;
@@ -23,7 +31,8 @@ document.getElementById('send-button').addEventListener('click', async () => {
                 })
             });
             const data = await response.json();
-            console.log(data.message);
+            console.log("Message - "+ data.message);
+            console.log("DB Response - "+ data.db_response);
             systemResponse.value = data.message;
         } catch (error) {
             console.error(error);
@@ -33,6 +42,37 @@ document.getElementById('send-button').addEventListener('click', async () => {
         validationText.style.display = 'block';
     }
 });
+
+
+// cleans the system response
+clearResponse.addEventListener('click', () => {
+    if (systemResponse.value) {
+        systemResponse.value = "";
+    }
+    else {
+        alert('Already cleared !')
+    }
+
+});
+
+
+// cleans the chat history on both temp. and perm. memeory(db)
+deleteCatMemory.addEventListener('click', async () => {
+    const URL = 'http://localhost:5000/clean-conversation-history';
+    const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    if (data.message == "history cleared") {
+        alert(data.message)
+    }
+    else {
+        alert(data.message)
+    }
+})
 
 
 
